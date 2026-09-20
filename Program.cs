09e13a8eef49b3754db68eec36a098b9e52883ee
Hlulani Tyrone Mthombeni <tshungutyrone@gmail.com>
2026-09-20 22:15:39 +0200

    Correct typo and remove unnecessary comments
    Fix typo in 'app.Environment' and clean up comments.

--- Program.cs
+++ Program.cs
@@ -1,18 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
-//
-if (app.Environment.IsDevelopment())
+
+if (app.Environent.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -24,5 +24,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-//
-//
